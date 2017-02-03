0c3bb697d6e162fab4047236e621378562b2ebf8
zhanghao <zhn_howl@qq.com>
2017-02-03 09:15:31 +0800

    添加项目文件。

---
+++ Parva.Lib.Updater/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parva.Lib.Updater
+{
+    public class Class1
+    {
+    }
+}
